d308b1fab582fce482a601c9e64c6c42cad17301
mpnri <mpnri2831@gmail.com>
2024-01-22 02:34:50 +0330

    Add SATLeanerSolver template + formulas tree template

--- src/solver.ts
+++ src/solver.ts
@@ -3,7 +3,22 @@ import { Formula, SATSolverType } from "./types";
 export const SAT_Solver = (
   formulas: Formula[],
   solverType: SATSolverType,
-): Formula | "unsatisfiable" => {
+): Formula | "unsatisfiable" | "solver_failed" => {
+  if (solverType === "linear") {
+    //todo: convert to a tree
+    return SATLeanerSolver(transformFormulas(formulas));
+  }
 
   return "unsatisfiable";
 };
+
+//* use the described Transform function(T(φ))
+function transformFormulas(formulas: Formula[]): Formula[] {
+  return formulas.map((formula) => formula.map((val) => -val).sort((a, b) => a - b));
+}
+
+//todo: convert to a tree
+function SATLeanerSolver(formulas: Formula[]): Formula | "unsatisfiable" | "solver_failed" {
+
+  return "solver_failed";
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,22 @@
 export type Formula = Number[];
 
-export type SATSolverType = "brute force" | "linear" | "cubic";
+//todo: use classes or namespaces?
+export type Node = VariableNode | AndNode | NotNode;
+
+export interface VariableNode {
+  type: "variable";
+  var: number;
+}
+
+export interface AndNode {
+  type: "and";
+  left: Node;
+  right: Node;
+}
+
+export interface NotNode {
+  type: "not";
+  child: Node;
+}
+
+export type SATSolverType = "brute_force" | "linear" | "cubic";
