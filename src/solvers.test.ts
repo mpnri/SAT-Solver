7fcb0cfc80374b5287eb022db9df368b43cc0faa
mpnri <mpnri2831@gmail.com>
2024-01-25 22:21:22 +0330

    Improve tests name

--- src/solvers.test.ts
+++ src/solvers.test.ts
@@ -2,8 +2,8 @@ import { describe, expect, it } from "vitest";
 import { SAT_Solver } from "./solver";
 import { SAT_Solver_Validator } from "./test.utils";
 
-describe("check brute_force SAT solver", () => {
-  it("should find answer", () => {
+describe("Check brute_force SAT solver", () => {
+  it("Should find answer", () => {
     const formulas = [[1], [-2], [-3]];
     const result = SAT_Solver(formulas, 3, "brute_force");
     expect(result).not.toBeTypeOf("string");
@@ -11,7 +11,7 @@ describe("check brute_force SAT solver", () => {
     expect(SAT_Solver_Validator(formulas, result)).toBe(true);
   });
 
-  it("should find answer", () => {
+  it("Should find answer", () => {
     const formulas = [
       [1, 2, 3],
       [1, -2],
@@ -24,7 +24,7 @@ describe("check brute_force SAT solver", () => {
     expect(SAT_Solver_Validator(formulas, result)).toBe(true);
   });
 
-  it("should response unsatisfiable", () => {
+  it("Should response unsatisfiable", () => {
     const formulas = [
       [1, 2, 3],
       [1, -2],
@@ -35,10 +35,22 @@ describe("check brute_force SAT solver", () => {
     const result = SAT_Solver(formulas, 3, "brute_force");
     expect(result).toBe<typeof result>("unsatisfiable");
   });
+
+  it("Should response unsatisfiable for p^(~p)", () => {
+    const formulas = [
+      [1],
+      [-1],
+      [2, -3],
+      [3, -1],
+      [-1, -2, -3],
+    ];
+    const result = SAT_Solver(formulas, 3, "brute_force");
+    expect(result).toBe<typeof result>("unsatisfiable");
+  });
 });
 
-describe("linear solver", () => {
-  it("should find answer", () => {
+describe("Check linear solver", () => {
+  it("Should find answer", () => {
     const result = SAT_Solver([[1], [-2], [-3]], 3, "linear");
     expect(result).toBeTypeOf("object");
     if (typeof result === "string") return;
@@ -46,18 +58,18 @@ describe("linear solver", () => {
     expect(is_same(result, [1, -2, -3])).toBe(true);
   });
 
-  it("should say unsatisfiable", () => {
+  it("Should say unsatisfiable", () => {
     const result = SAT_Solver([[1], [-1]], 1, "linear");
     expect(result).toBe<ReturnType<typeof SAT_Solver>>("unsatisfiable");
   });
 
-  it("could not solve this", () => {
+  it("Could not solve this", () => {
     const result = SAT_Solver([[1], [-1, 2]], 2, "linear");
     expect(result).toBe<ReturnType<typeof SAT_Solver>>("solver_failed");
   });
 });
 
-describe("cubic solver", () => {
+describe("Check cubic solver", () => {
   it(`p^(~q)^(~r) should be ${[[1, -2, -3]]}`, () => {
     const result = SAT_Solver([[1], [-2], [-3]], 3, "cubic");
     expect(result).toEqual<typeof result>([1, -2, -3]);
