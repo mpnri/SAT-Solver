d75e0b0988cb928e069e9ce65a8fa8baedcaca0e
mpnri <mpnri2831@gmail.com>
2024-01-25 14:25:06 +0330

    Fix modifying existed value in value function

--- src/solver.ts
+++ src/solver.ts
@@ -24,7 +24,7 @@ function SATLeanerSolver(rootNode: Node): Formula | "unsatisfiable" | "solver_fa
   while (queue.length > 0) {
     const { node, value } = queue.shift();
     if (node.type === "variable") {
-      if (valFunc.get(node.var) && valFunc.get(node.var) !== value) {
+      if (valFunc.has(node.var) && valFunc.get(node.var) !== value) {
         return "unsatisfiable";
       }
       valFunc.set(node.var, value);
@@ -49,6 +49,7 @@ function SATCubicSolver(rootNode: Node): Formula | "unsatisfiable" | "solver_fai
   const queue: { node: Node; value: boolean }[] = [{ node: rootNode, value: true }];
   const valueFunc = new Map<number, boolean>();
   if (BasicCubicSolver(queue, valueFunc)) {
+    // console.log("answer", [...valueFunc.entries()]);
     return Array.from(valueFunc.entries())
       .sort((a, b) => a[0] - b[0])
       .map(([variable, value]) => variable * (value ? +1 : -1));
@@ -62,9 +63,9 @@ function BasicCubicSolver(
   while (queue.length > 0) {
     const { node, value: tempValue } = queue.shift();
     if (node.type === "variable") {
-      if (valueFunc.get(node.var) && valueFunc.get(node.var) !== tempValue) {
-        console.log("variable error");
-        console.log(node.var, tempValue);
+      if (valueFunc.has(node.var) && valueFunc.get(node.var) !== tempValue) {
+        // console.log("variable error");
+        // console.log(node.var, tempValue);
         // console.log("valueFunc says", valueFunc.get(node.var))
         return false;
       }
@@ -77,14 +78,19 @@ function BasicCubicSolver(
       if (tempValue === true) {
         queue.push({ node: node.left, value: true }, { node: node.right, value: true });
       } else {
-        printTree(node);
-        console.log("-----");
-        console.log("original", [...valueFunc.entries()]);
+        // console.log("CHECK");
+        // printTree(node);
+        // console.log("-----");
+
         for (let children of [
           { left: true, right: false },
           { left: false, right: true },
           { left: false, right: false },
         ]) {
+          // console.log("original", [...valueFunc.entries()]);
+          // console.log("check case", children);
+          // printTree(node);
+          // console.log("++++");
           // const tempValueFunc = new Map(valueFunc);
           const tempValueFunc = new Map<number, boolean>();
           valueFunc.forEach((value, key) => tempValueFunc.set(key, value));
@@ -95,14 +101,16 @@ function BasicCubicSolver(
             { node: node.right, value: children.right },
           );
           if (BasicCubicSolver(tempQueue, tempValueFunc)) {
+            // console.log("ok origin", [...valueFunc.entries()]);
             tempValueFunc.forEach((value, key) => valueFunc.set(key, value));
-            console.log("ok", [...tempValueFunc.entries()]);
+            // console.log("ok", [...valueFunc.entries()]);
+            // console.log("ok", [...tempValueFunc.entries()]);
             return true;
           }
-          console.log("not ok", [...tempValueFunc.entries()]);
+          // console.log("not ok", [...tempValueFunc.entries()]);
         }
-        console.log("false");
-        console.log("-----");
+        // console.log("false");
+        // console.log("-----");
         return false;
       }
     }
